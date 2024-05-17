d0a800745103ca9e0bc56c20a55acb5f099a3bfc
rezyanto263 <rezyanto263@gmail.com>
2024-05-17 18:57:15 +0800

    hero section progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 document.addEventListener("scroll", () => {
     const navbar = document.querySelector("nav");
 
-    if (window.scrollY > 160) {
+    if (window.scrollY > 95) {
         navbar.classList.remove("hidden");
     }else {
         navbar.classList.add("hidden");
