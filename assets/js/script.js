eaaa58963b338f84bc9f54b24d549978f21381fc
rezyanto263 <rezyanto263@gmail.com>
2024-06-07 23:14:30 +0800

    fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,39 @@
 // Navbar Show Up and Hide In
 navhidden = document.querySelector(".hidden")
 if (navhidden != null) {
-document.addEventListener("scroll", () => {
-	const navbar = document.querySelector(".navbar");
+  document.addEventListener("scroll", () => {
+    const navbar = document.querySelector(".navbar");
 
-	if (window.scrollY > 95) {
-		navbar.classList.remove("hidden");
-	} else {
-		navbar.classList.add("hidden");
-	}
+    if (window.scrollY > 95) {
+      navbar.classList.remove("hidden");
+    } else {
+      navbar.classList.add("hidden");
+    }
 });
+}
+
+// Booking Max People
+check = document.querySelector(".adult");
+if(check!=null) {
+const plus = document.querySelector(".plus"),
+minus = document.querySelector(".minus"),
+num = document.querySelector(".num")
+
+  a = 1;
+
+  plus.addEventListener("click", ()=>{
+  a++;
+  a = (a < 10) ? "0" + a : a;
+  num.innerText = a;
+  });
+
+  minus.addEventListener("click", ()=>{
+      if(a > 1){
+          a--;
+          a = (a < 10) ? "0" + a : a;
+  num.innerText = a;
+      }
+  });
 }
 
 // Dashboard SideBar
@@ -24,9 +48,11 @@ if (sidebarbtn != null) {
 // Owl Carousel
 videoSlider = document.querySelector(".video-slider");
 teamSlider = document.querySelector(".video-slider");
+boatOverviewSlider = document.querySelector(".boat-overview-slider");
 
-if ((videoSlider && teamSlider) != null) {
+if ((videoSlider || teamSlider || boatOverviewSlider) != null) {
 	$(document).ready(function () {
+    
 		$(".video-slider").owlCarousel({
 			loop: false,
 			margin: 24,
@@ -35,12 +61,25 @@ if ((videoSlider && teamSlider) != null) {
 			autoWidth: true,
 		});
 
-		$(".teams-slider").owlCarousel({
-			loop: false,
-			margin: 24,
-			nav: false,
-			dots: false,
-			autoWidth: true,
-		});
-	});
+    $(".teams-slider").owlCarousel({
+      loop:false,
+      margin:24,
+      nav:false,
+      dots:false,
+      autoWidth:true,
+    });
+
+    $(".boat-overview-slider").owlCarousel({
+      loop:true,
+      autoplay: false,
+      items: 1,
+      margin: 24,
+      nav: false,
+      center: true,
+      dots:true,
+    });
+
+  });
 }
+
+
