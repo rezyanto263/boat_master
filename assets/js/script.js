98d389741aad649c7f665b1c6449dc7c8e2b2ba5
Ayu Lestari <xxyules@gmail.com>
2024-06-07 19:39:52 +0800

    Booking Page Progress Fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,4 +26,42 @@ $(document).ready(function(){
         dots:false,
         autoWidth:true,
     });
+
+    $(".boat-overview-slider").owlCarousel({
+        loop:true,
+        autoplay: false,
+        items: 1,
+        margin: 24,
+        nav: false,
+        center: true,
+        dots:true,
+
+    });
+
+
 });
+
+
+const plus = document.querySelector(".plus"),
+minus = document.querySelector(".minus"),
+num = document.querySelector(".num");
+
+if ((plus&&minus&&num)!=null) {
+let a = 1;
+
+plus.addEventListener("click", ()=>{
+a++;
+a = (a < 10) ? "0" + a : a;
+num.innerText = a;
+console.log(a);
+});
+
+minus.addEventListener("click", ()=>{
+    if(a > 1){
+        a--;
+        a = (a < 10) ? "0" + a : a;
+num.innerText = a;
+console.log(a);
+    }
+});
+}
