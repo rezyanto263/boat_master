5572c13c4fc26f5cb2163dade226d1dcc947f03e
rezyanto263 <rezyanto263@gmail.com>
2024-07-30 16:42:41 +0800

    Fix some rensponsive

--- application/views/partials/user/navbar.php
+++ application/views/partials/user/navbar.php
@@ -4,16 +4,6 @@
         <a class="navbar-brand d-flex align-items-center" href="<?= base_url('home'); ?>">
             <img src="<?= base_url('assets/images/logo.png'); ?>" alt="" width="55px" />
         </a>
-        <button
-            class="btn"
-            id="offcanvas-navbar"
-            type="button"
-            data-bs-toggle="offcanvas"
-            data-bs-target="#offcanvas-navbar"
-            aria-controls="offcanvasExample"
-        >
-            <i class="fa-solid fa-bars"></i>
-        </button>
         <ul class="navbar-nav mx-auto d-flex grid gap-5">
             <li class="nav-item">
                 <a class="nav-link" href="<?= base_url('home'); ?>">Home</a>
@@ -31,27 +21,65 @@
                 <a class="nav-link" href="<?= base_url('contact'); ?>">Contact</a>
             </li>
         </ul>
-        <a class="btn btn-secondary btn-signin max navbar-nav" href="<?= base_url('login'); ?>">
-            SIGN IN
-        </a>
-        <div class="wrapper btn-icon d-flex flex-row">
-            <a
-                class="tickets d-flex align-items-center justify-content-center me-3 position-relative notification"
-                href="<?= base_url('tickets') ?>"
+        <div class="d-flex">
+            <button
+                class="btn"
+                id="offcanvas-navbar"
+                type="button"
+                data-bs-toggle="offcanvas"
+                data-bs-target="#offcanvas-menu"
             >
-                <i class="fa-solid fa-ticket"></i>
-                <span class="notif-circle text-center">
-                    <i class="notif-icon fa-solid fa-exclamation"></i>
-                </span>
+                <i class="fa-solid fa-bars"></i>
+            </button>
+            <a class="btn btn-secondary btn-signin max" href="<?= base_url('login'); ?>">
+                SIGN IN
             </a>
-            <a
-                class="profile-user d-flex align-items-center justify-content-center"
-                href="<?= base_url('profile'); ?>"
-            >
-                <?= empty($this->session->userdata('custPicture'))?'<i class="fa-solid fa-circle-user"></i>':'<img class="profile-image" src="'.base_url('assets/uploads/'.$this->session->userdata('custPicture')).'">'; ?>
-            </a>
-            
+            <div class="btn-icon d-flex">
+                <a
+                    class="tickets d-flex align-items-center justify-content-center me-3 position-relative notification"
+                    href="<?= base_url('tickets') ?>"
+                >
+                    <i class="fa-solid fa-ticket"></i>
+                    <span class="notif-circle text-center">
+                        <i class="notif-icon fa-solid fa-exclamation"></i>
+                    </span>
+                </a>
+                <a
+                    class="profile-user d-flex align-items-center justify-content-center"
+                    href="<?= base_url('profile'); ?>"
+                >
+                    <?= empty($this->session->userdata('custPicture'))?'<i class="fa-solid fa-circle-user"></i>':'<img class="profile-image" src="'.base_url('assets/uploads/'.$this->session->userdata('custPicture')).'">'; ?>
+                </a>
+            </div>
         </div>
     </div>
 </nav>
+
+<div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvas-menu">
+    <div class="offcanvas-header">
+        <img src="<?= base_url('assets/images/logo.png')?>" width="50">
+        <h4 class="offcanvas-title ms-3">MENU</h4>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <hr class="offcanvas-divider m-0">
+    <div class="offcanvas-body w-100">
+        <ul class="offcanvas-menu ps-0">
+            <li class="offcanvas-item">
+                <a href="<?= base_url('home'); ?>" class="offcanvas-link <?= $title == 'Home'?'active':'' ?>">HOME</a>
+            </li>
+            <li class="offcanvas-item">
+                <a href="<?= base_url('boats'); ?>" class="offcanvas-link <?= $title == 'Boats'?'active':'' ?>">BOATS</a>
+            </li>
+            <li class="offcanvas-item">
+                <a href="<?= base_url('gallery'); ?>" class="offcanvas-link <?= $title == 'Gallery'?'active':'' ?>">GALLERY</a>
+            </li>
+            <li class="offcanvas-item">
+                <a href="<?= base_url('about'); ?>" class="offcanvas-link <?= $title == 'About'?'active':'' ?>">ABOUT</a>
+            </li>
+            <li class="offcanvas-item">
+                <a href="<?= base_url('contact'); ?>" class="offcanvas-link <?= $title == 'Contact'?'active':'' ?>">CONTACT</a>
+            </li>
+        </ul>
+    </div>
+</div>
 <!-- Navbar End -->
